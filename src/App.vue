a4c940c741a5750ee1e40b9272f5902e97a10f89
Delvin <141915380+delvin-monash@users.noreply.github.com>
2024-08-14 01:55:30 +1000

    Upgrade 4: Added Login feature. User cannot enter site without logging in. Using vue-router + emits (events) to achieve this.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { useToast } from 'primevue/usetoast'
+import { ref } from 'vue'
 const toast = useToast()
 
 const showSuccess = (title, description) => {
@@ -21,34 +22,45 @@ import FooterSection from '@/components/FooterSection.vue'
 import HomeView from './views/HomeView.vue'
 import HeroSection from './components/HeroSection.vue'
 import Toast from 'primevue/toast'
+import LoginView from './views/LoginView.vue'
+
+const isAuthenticated = ref(false)
+
+const handleAuthentication = (value) => {
+  isAuthenticated.value = value
+}
 </script>
 
 <template>
   <Toast />
+  <div v-if="isAuthenticated == true">
+    <HeaderSection />
 
-  <HeaderSection />
+    <!-- Hide this if the current page is not the Home page -->
+    <HeroSection v-if="$route.path === '/'" />
 
-  <!-- Hide this if the current page is not the Home page -->
-  <HeroSection v-if="$route.path === '/'" />
-
-  <div class="container">
-    <div class="row">
-      <div class="col-9 main-content">
-        <RouterView />
-      </div>
-      <div class="col-3 sidebar">
-        <button
-          type="button"
-          class="btn btn-primary"
-          @click="showSuccess('Success', 'You now know how to get a toast working!')"
-        >
-          Show Toast
-        </button>
-        <HealthyEatingTips />
+    <div class="container">
+      <div class="row">
+        <div class="col-9 main-content">
+          <RouterView />
+        </div>
+        <div class="col-3 sidebar">
+          <button
+            type="button"
+            class="btn btn-primary"
+            @click="showSuccess('Success', 'You now know how to get a toast working!')"
+          >
+            Show Toast
+          </button>
+          <HealthyEatingTips />
+        </div>
       </div>
     </div>
+    <FooterSection />
   </div>
-  <FooterSection />
+  <div v-else>
+    <LoginView @authenticated="handleAuthentication" />
+  </div>
 </template>
 
 <style scoped></style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,94 @@
+<script setup>
+import { useToast } from 'primevue/usetoast'
+import { useRouter } from 'vue-router'
+import { defineEmits } from 'vue'
+
+const router = useRouter()
+const toast = useToast()
+
+const emit = defineEmits(['authenticated'])
+
+const showError = () => {
+  toast.add({
+    severity: 'error',
+    summary: 'Incorrect username/password. Check the hint!',
+    detail: 'Try again pls.',
+    life: 3000
+  })
+}
+
+const globalUsername = 'admin@gmail.com'
+const globalPassword = 'password'
+
+const handleSubmit = (event) => {
+  event.preventDefault()
+  const email = event.target.email.value
+  const password = event.target.password.value
+
+  if (email === globalUsername && password === globalPassword) {
+    console.log('Login successful')
+    emit('authenticated', true)
+    router.push('/')
+  } else {
+    console.log('Login failed')
+    showError()
+  }
+}
+</script>
+
+<template>
+  <div class="login-container">
+    <div class="login-box text-center">
+      <img src="@/assets/logo.svg" alt="Bootstrap Logo" class="logo" width="72" height="57" />
+      <h2 class="mb-4">Sign In</h2>
+      <p class="hint">Hint: The username is 'admin@gmail.com' and the password is 'password'.</p>
+      <form @submit.prevent="handleSubmit">
+        <div class="mb-3">
+          <label for="email" class="form-label">Email address</label>
+          <input
+            type="email"
+            class="form-control"
+            id="email"
+            aria-describedby="emailHelp"
+            required
+          />
+        </div>
+        <div class="mb-3">
+          <label for="password" class="form-label">Password</label>
+          <input type="password" class="form-control" id="password" required />
+        </div>
+        <div class="mb-3 form-check text-start">
+          <input type="checkbox" class="form-check-input" id="rememberMe" />
+          <label class="form-check-label" for="rememberMe">Remember me</label>
+        </div>
+        <button type="submit" class="btn btn-primary w-100">Sign In</button>
+      </form>
+      <div class="mt-3">
+        <a href="#" class="link-secondary">Forgot my password</a> |
+        <a href="#" class="link-secondary">Create an account</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.login-box {
+  max-width: 360px;
+  margin: auto;
+  padding: 15px;
+  border: 1px solid #e3e3e3;
+  border-radius: 10px;
+  background-color: #ffffff;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+.login-container {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.logo {
+  display: block;
+  margin: 0 auto 20px;
+}
+</style>
